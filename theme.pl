8038af92f8e76fd0dfaeade59d24c824c0685c31
Riccardo Nobile <blackhardware@gmail.com>
2014-01-12 20:38:04 +0100

    Theme Update - 12/01/2014
    Updating the functions file (theme.pl) of the template and fixed some
    bugs in CSS file (default.css)

--- theme.pl
+++ theme.pl
@@ -16,6 +16,8 @@ sub theme_header {
 if (@_ > 1) {
 	print '<div class="container">' . "\n";
 	my %this_module_info = &get_module_info(&get_module_name());
+	print '<div class="panel panel-default" style="margin-top: 20px;">' . "\n";
+	print '<div class="panel-heading">' . "\n";
 	print "<table class='header' width=100%><tr>\n";
 	if ($gconfig{'sysinfo'} == 2 && $remote_user) {
 		print "<td id='headln1' colspan=3 align=center>\n";
@@ -92,6 +94,8 @@ if (@_ > 1) {
 	print $_[6];
 	print "</td></tr></table>\n";
 	print $tconfig{'postheader'};
+	print '</div>' . "\n";
+	print '<div class="panel-body">' . "\n";
 	}
 $miniserv::page_capture = 1;
 }
@@ -110,64 +114,13 @@ for(my $i=0; $i+1<@_; $i+=2) {
 			$url = "/".&get_module_name()."/$url";
 			}
 		$url = "$gconfig{'webprefix'}$url" if ($url =~ /^\//);
-		print "&nbsp;<a href=\"$url\"><i class='fa fa-arrow-left'></i> ",&text('main_return', $_[$i+1]),"</a>\n";
+		print "&nbsp;<a style='margin-bottom: 15px;' class='btn btn-primary' href=\"$url\"><i class='fa fa-arrow-left'></i> ",&text('main_return', $_[$i+1]),"</a>\n";
 		}
 	}
-	#If you comment this decomment the </div> in theme_ui_pre_footer, index.cgi, body.cgi
+	#If you comment this </div>, decomment the </div> commented in theme_ui_pre_footer, index.cgi, body.cgi
 	print "</div>\n";
 	print '</body>' , "\n";
 	print '</html>' , "\n";
-}
-sub theme_ui_tabs_start {
-	my ($tabs, $name, $sel, $border) = @_;
-	my $rv;
-	# Output the tabs
-	my $imgdir = "$gconfig{'webprefix'}/images";
-	$rv .= &ui_hidden($name, $sel)."\n";
-	$rv .= '<ul class="nav nav-tabs">' . "\n";
-	foreach my $t (@$tabs) {
-		my $tabid = "tab_".$t->[0];
-		if ($t->[0] eq $sel) {
-			# Selected tab
-			$rv .= '<li class="active"><a data-toggle="tab" href="#' . $t->[0] . '">' . $t->[1] . '</a></li>' . "\n";
-		}
-		else {
-			# Other tab (which has a link)
-			$rv .= '<li><a data-toggle="tab" href="#' . $t->[0] . '">' . $t->[1] . '</a></li>' . "\n";
-		}
-	}
-	$rv .= '</ul>' . "\n";
-	$rv .= '<div class="tab-content">' . "\n";
-	$main::ui_tabs_selected = $sel;
-	return $rv;
-}
-sub theme_ui_tabs_end {
-my $rv;
-$rv .= '</div>' . "\n";
-return $rv;
-}
-sub theme_ui_tabs_start_tab {
-	my ($name, $tab) = @_;
-	my $defclass = $tab eq $main::ui_tabs_selected ?
-				' active' : '';
-	my $rv = "<div id='$tab' class='tab-pane$defclass'>\n";
-	return $rv;
-}
-
-sub theme_ui_tabs_end_tab {
-	return "</div>\n";
-}
-sub theme_ui_post_header {
-	my ($text) = @_;
-	my $rv;
-	$rv .= "<hr>\n";
-	return $rv;
-}
-sub theme_ui_pre_footer {
-	my $rv;
-	$rv .= "<hr>\n";
-	#$rv .= '</div>' . "\n";
-	return $rv;
 }
 
 sub theme_generate_icon {
@@ -184,23 +137,187 @@ if ($tconfig{'noicons'}) {
 		}
 	}
 elsif ($_[2]) {
-	print "<table><tr><td width=48 height=48>\n",
+	print "<table><tr><td>\n",
 	      "<a href=\"$_[2]\" $_[3]><img src=\"$icon\" alt=\"\" border=0 ",
 	      "$w $h></a></td></tr></table>\n";
 	print "$_[6]<a href=\"$_[2]\" $_[3]>$_[1]</a>$_[7]\n";
 	}
 else {
-	print "<table><tr><td width=48 height=48>\n",
+	print "<table><tr><td>\n",
 	      "<img src=\"$icon\" alt=\"\" border=0 $w $h>",
 	      "</td></tr></table>\n$_[6]$_[1]$_[7]\n";
 	}
 }
 
+sub theme_file_chooser_button
+{
+my $form = defined($_[2]) ? $_[2] : 0;
+my $chroot = defined($_[3]) ? $_[3] : "/";
+my $add = int($_[4]);
+my ($w, $h) = (400, 300);
+if ($gconfig{'db_sizefile'}) {
+	($w, $h) = split(/x/, $gconfig{'db_sizefile'});
+	}
+return "<input class='btn btn-default' type=button onClick='ifield = form.$_[0]; chooser = window.open(\"$gconfig{'webprefix'}/chooser.cgi?add=$add&type=$_[1]&chroot=$chroot&file=\"+escape(ifield.value), \"chooser\", \"toolbar=no,menubar=no,scrollbars=no,resizable=yes,width=$w,height=$h\"); chooser.ifield = ifield; window.ifield = ifield' value=\"...\">\n";
+}
+sub theme_popup_window_button
+{
+my ($url, $w, $h, $scroll, $fields) = @_;
+my $scrollyn = $scroll ? "yes" : "no";
+my $rv = "<input class='btn btn-default' type=button onClick='";
+foreach my $m (@$fields) {
+	$rv .= "$m->[0] = form.$m->[1]; ";
+	}
+my $sep = $url =~ /\?/ ? "&" : "?";
+$rv .= "chooser = window.open(\"$url\"";
+foreach my $m (@$fields) {
+	if ($m->[2]) {
+		$rv .= "+\"$sep$m->[2]=\"+escape($m->[0].value)";
+		$sep = "&";
+		}
+	}
+$rv .= ", \"chooser\", \"toolbar=no,menubar=no,scrollbars=$scrollyn,resizable=yes,width=$w,height=$h\"); ";
+foreach my $m (@$fields) {
+	$rv .= "chooser.$m->[0] = $m->[0]; ";
+	$rv .= "window.$m->[0] = $m->[0]; ";
+	}
+$rv .= "' value=\"...\">";
+return $rv;
+}
+
+
+# Thi is the theme.pl part dedicated to all theme functions.
+# WARNING!!! Work in progress - Not all is implemented!!!
+# All the # theme_ui_x are are not yet implemented theme functions.
+
+################################# Theme table generation functions #################################
+
+# theme_ui_table_start
+
+# theme_ui_table_end
+
+# theme_ui_table_row
+
+# theme_ui_table_hr
+
+# theme_ui_table_span
+
+# theme_ui_columns_start
+
+# theme_ui_columns_row
+
+# theme_ui_columns_header
+
+# theme_ui_checked_columns_row
+
+# theme_ui_radio_columns_row
+
+# theme_ui_columns_end
+
+# theme_ui_columns_table
+
+# theme_ui_form_columns_table
+
+################################# Theme form generation functions #################################
+
+sub theme_ui_form_start {
+	my ($script, $method, $target, $tags) = @_;
+	my $rv;
+	
+	$rv .= '<form role="form" ';
+	$rv .= 'action="' . &html_escape($script) . '" ';
+	$rv .= ($method eq 'post' ? 'method="post" ' : $method eq 'form-data' ? 'method="post" enctype="multipart/form-data" ' : 'method="get" ');
+	$rv .= ($target ? 'target="' . $target . '" ' : '');
+	$rv .= ($tags ? $tags : '');
+	$rv .= '>' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_form_end
+
+sub theme_ui_textbox {
+	my ($name, $value, $size, $dis, $max, $tags) = @_;
+	my $rv;
+	
+	$rv .= '<input style="display: inline; width: auto;" class="form-control" type="text" ';
+	$rv .= 'name="' . &quote_escape($name) . '" ';
+	$rv .= 'value="' . &quote_escape($value) . '" ';
+	$rv .= 'size="' . $size . '" ';
+	$rv .= ($dis ? 'disabled="true" ' : '');
+	$fv .= ($max ? 'maxlength="' . $max . '" ' : '');
+	$rv .= ($tags ? $tags : '');
+	$rv .= '>' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_filebox
+
+# theme_ui_upload
+
+sub theme_ui_password {
+	my ($name, $value, $size, $dis, $max, $tags) = @_;
+	my $rv;
+	
+	$rv .= '<input style="display: inline; width: auto;" class="form-control" type="password" ';
+	$rv .= 'name="' . &quote_escape($name) . '" ';
+	$rv .= 'value="' . &quote_escape($value) . '" ';
+	$rv .= 'size="' . $size . '" ';
+	$rv .= ($dis ? 'disabled="true" ' : '');
+	$fv .= ($max ? 'maxlength="' . $max . '" ' : '');
+	$rv .= ($tags ? $tags : '');
+	$rv .= '>' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_hidden
+
+# theme_ui_select
+
+# theme_ui_multi_select
+
+# theme_ui_multiselect_javascript
+
+# theme_ui_radio
+
+# theme_ui_yesno_radio
+
+# theme_ui_checkbox
+
+# theme_ui_oneradio
+
+sub theme_ui_textarea {
+	my ($name, $value, $rows, $cols, $wrap, $dis, $tags) = @_;
+	my $rv;
+	$cols = &ui_max_text_width($cols, 1);
+	
+	$rv .= '<textarea style="display: inline; width: auto;" class="form-control" ';
+	$rv .= 'name="' . &quote_escape($name) . '" ';
+	$rv .= 'rows="' . $rows . '" ';
+	$rv .= 'cols="' . $cols . '" ';
+	$rv .= ($wrap ? 'wrap="' . $wrap . '" ' : '');
+	$rv .= ($dis ? 'disabled="true" ' : '');
+	$rv .= ($tags ? $tags : '');
+	$rv .= '>' . "\n";
+	$rv .= &html_escape($value) . "\n";
+	$rv .= '</textarea>' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_user_textbox
+
+# theme_ui_group_textbox
+
+# theme_ui_opt_textbox
+
 sub theme_ui_submit {
 	my ($label, $name, $dis, $tags) = @_;
-	my $rv;
-	my $fa;
+	my ($rv, $fa);
 	my $btntype = 'btn-default';
+	 
 	if ($name eq 'delete') {
 		$btntype = 'btn-danger';
 		#$fa = '<i class="fa fa-times"></i>';
@@ -214,37 +331,172 @@ sub theme_ui_submit {
 		$btntype = 'btn-warning';
 		#$fa = '<i class="fa fa-refresh"></i>';
 	}
+	
 	$rv .= '<button type="submit" class="btn ' . $btntype . '" ';
 	$rv .= ($name ne '' ? 'name="' . &quote_escape($name) . '" ' : '');
 	$rv .= ($dis ? ' disabled="disabled"' : '');
 	$rv .= ($tags ? ' ' . $tags : ''). '>';
 	$rv .= $fa . ' ' . &quote_escape($label);
-	$rv .= '</button>';
-	$rv .= "\n";
+	$rv .= '</button>' . "\n";
+	
 	return $rv;
 }
+
 sub theme_ui_reset {
 	my ($label, $dis) = @_;
 	my $rv;
+	
 	$rv .= '<button class="btn btn-default" type="reset" ';
 	$rv .= ($dis ? 'disabled="disabled">' : '>');
 	$rv .= &quote_escape($label);
-	$rv .= '</button>';
-	$rv .= "\n";
+	$rv .= '</button>' . "\n";
+	
 	return $rv;
 }
+
 sub theme_ui_button {
 	my ($label, $name, $dis, $tags) = @_;
 	my $rv;
-	$rv .= '<button class="btn btn-default" type="button" ';
+	
+	$rv .= '<button type="button" class="btn btn-default" ';
 	$rv .= ($name ne '' ? 'name="' . &quote_escape($name) . '" ' : '');
 	$rv .= ($dis ? 'disabled="disabled"' : '');
-	$rv .= ($tags ? ' ' . $tags : ''). '>'
+	$rv .= ($tags ? ' ' . $tags : ''). '>';
 	$rv .= &quote_escape($label);
-	$rv .= '</button>';
-	$rv .= "\n";
+	$rv .= '</button>' . "\n";
+	
 	return $rv;
 }
+
+# theme_ui_date_input
+
+# theme_ui_buttons_start
+
+# theme_ui_buttons_end
+
+# theme_ui_buttons_row
+
+# theme_ui_buttons_hr
+
+################################# Theme header and footer functions #################################
+
+sub theme_ui_post_header {
+	my ($text) = @_;
+	
+	my $rv;
+	#$rv .= '<hr>' . "\n";
+	
+	return $rv;
+}
+
+sub theme_ui_pre_footer {
+	my $rv;
+	
+	#$rv .= '<hr>' . "\n";
+	$rv .= '</div>' . "\n";
+	$rv .= '</div>' . "\n";
+	#$rv .= '</div>' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_print_header
+
+# theme_ui_print_unbuffered_header
+
+# theme_ui_print_footer
+
+# theme_ui_config_link
+
+# theme_ui_print_endpage
+
+# theme_ui_subheading
+
+# theme_ui_links_row
+
+################################# Theme collapsible section / tab functions #################################
+
+# theme_ui_hidden_javascript
+
+# theme_ui_hidden_start
+
+# theme_ui_hidden_end
+
+# theme_ui_hidden_table_row_start
+
+# theme_ui_hidden_table_row_end
+
+# theme_ui_hidden_table_start
+
+# theme_ui_hidden_table_end
+
+sub theme_ui_tabs_start {
+	my ($tabs, $name, $sel, $border) = @_;
+	my $rv;
+	
+	$rv .= '<ul class="nav nav-tabs">' . "\n";
+	foreach my $t (@$tabs) {
+		my $tabid = "tab_".$t->[0];
+		if ($t->[0] eq $sel) {
+			$rv .= '<li class="active"><a data-toggle="tab" href="#' . $t->[0] . '">' . $t->[1] . '</a></li>' . "\n";
+		} else {
+			$rv .= '<li><a data-toggle="tab" href="#' . $t->[0] . '">' . $t->[1] . '</a></li>' . "\n";
+		}
+	}
+	$rv .= '</ul>' . "\n";
+	$rv .= '<div class="tab-content">' . "\n";
+	$main::ui_tabs_selected = $sel;
+	
+	return $rv;
+}
+
+sub theme_ui_tabs_end {
+	my ($border) = @_;
+	my $rv;
+	
+	$rv .= '</div>' . "\n";
+	
+	return $rv;
+}
+
+sub theme_ui_tabs_start_tab {
+	my ($name, $tab) = @_;
+	my $rv;
+	my $defclass = $tab eq $main::ui_tabs_selected ? 'active' : '';
+	
+	$rv .= '<div id="' . $tab . '" class="tab-pane ' . $defclass . '">' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_tabs_start_tabletab
+
+sub theme_ui_tabs_end_tab {
+	my $rv;
+	
+	$rv .= '</div>' . "\n";
+	
+	return $rv;
+}
+
+# theme_ui_tabs_end_tabletab
+
+# theme_ui_radio_selector
+
+################################# Theme collapsible section / tab functions #################################
+
+# theme_ui_grid_table
+
+# theme_ui_radio_table
+
+# theme_ui_up_down_arrows
+
+# theme_ui_hr
+
+# theme_ui_nav_link
+
+# theme_ui_confirmation_form !!!!! NB: Not available to override ?????
+
 sub theme_ui_alert_box {
 	my ($msg, $class) = @_;
 	my ($rv, $type, $tmsg, $fa);
@@ -258,8 +510,11 @@ sub theme_ui_alert_box {
 	$rv .= '<i class="fa fa-fw ' . $fa . '"></i> <strong>' . $tmsg . '</strong>';
 	$rv .= '<br>' . "\n";
 	$rv .= $msg . "\n";
-	$rv .= '</div>';
-	$rv .= "\n";
+	$rv .= '</div>' . "\n";
 	
 	return $rv;
 }
+
+# theme_ui_page_flipper
+
+# theme_js_redirect
